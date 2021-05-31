d669260bf2489c271b9e15eafdf34f0fd91d0e64
Metehan Karaşahinoğlu <mthn.karasahin@hotmail.com>
2021-05-31 04:42:37 +0300

    -Category Authentication is added.    -ToDo Edit/Create Droplist was showing all categories from all users. [Fixed]    -CategoryMenu was showing all items from all categories instead of the specific ones relevant to specific category name. [Fixed]

---
+++ CetTodoWeb/ViewComponents/CategoryMenuViewComponent.cs
@@ -0,0 +1,43 @@
+﻿using CetTodoWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CetTodoWeb.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+namespace CetTodoWeb.ViewComponents
+{
+    
+    public class CategoryMenuViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext dbContext;
+        private readonly UserManager<CetUser> _userManager;
+        public CategoryMenuViewComponent(ApplicationDbContext dbContext, UserManager<CetUser> userManager)
+        {
+            this.dbContext = dbContext;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(bool ShowEmpty=true)
+        {
+            List<Category> items;
+            if(User.Identity.IsAuthenticated)
+			{
+                var currentUser = await _userManager.GetUserAsync(HttpContext.User);
+                var query = dbContext.Categories.Where(t => t.CetUserId == currentUser.Id);
+                items = await query.Where(c => ShowEmpty || c.TodoItems.Any()).ToListAsync();
+            }
+            else
+			{
+                items = await dbContext.Categories.Where(c => ShowEmpty).ToListAsync();
+            }
+            //var items = await dbContext.Categories.Where(c => ShowEmpty || c.TodoItems.Any()).ToListAsync();
+            
+            
+            return View(items);
+        }
+    }
+}
